a5439bb199358f6c36e978bab3c7a54c6f0a1228
vineetverj <vineet638796@gmail.com>
2024-08-13 18:32:50 +0530

    first commit

---
+++ flutter_application_1/lib/calculator.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Calculatorapp extends StatefulWidget {
+  const Calculatorapp({super.key});
+
+  @override
+  State<Calculatorapp> createState() => _CalculatorappState();
+}
+
+class _CalculatorappState extends State<Calculatorapp> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
